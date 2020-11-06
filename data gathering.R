68df1068e7eb3bb752e07c7d6086a6fd711ae169
danbaissa <danbaissa@gmail.com>
2020-11-06 10:15:44 -0500

    Fixed

--- data gathering.R
+++ data gathering.R
@@ -2,21 +2,31 @@ library(dplyr)
 library(tidyverse)
 library(readxl)
 library(plyr)
+library(stringr)
 
 NFL_2016_label <- read_excel("raw_data/NFL_2016_label.xlsx", 
                              skip = 3)
 NFL_2016 <- NFL_2016_label%>%
   mutate(year = 2016)
 
+NFL_2016$Player <- word(NFL_2016$Player, 1,2, sep=" ")
+
+
 NFL_2017_label <- read_excel("raw_data/NFL_2017_label.xlsx", 
                              skip = 3)
 NFL_2017 <- NFL_2017_label%>%
   mutate(year = 2017)
 
+NFL_2017$Player <- word(NFL_2017$Player, 1,2, sep=" ")
+
+
 NFL_2018_label <- read_excel("raw_data/NFL_2018_label.xlsx", 
                              skip = 3)
 NFL_2018 <- NFL_2018_label%>%
   mutate(year = 2018)
+
+NFL_2018$Player <- word(NFL_2018$Player, 1,2, sep=" ")
+
 
 NFL_Full_stat <- join(NFL_2016,NFL_2017,by = "year", type = "full")
 NFL_Full_stat <- join(NFL_Full_stat,NFL_2018,by = "year", type = "full")
